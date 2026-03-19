fc07e0c6330bb344f7073ca2e99c016a14abe17f
Aditya <adityapandey2318@gmail.com>
2026-03-19 14:41:37 +0530

    Added Loops Practice Question

---
+++ 08--Loops and Sequence/Q2.py
@@ -0,0 +1,9 @@
+# Write a program to find whether a given number is prime or not.
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
---
+++ 08--Loops and Sequence/Q3.py
@@ -0,0 +1,6 @@
+#Factorial of n 
+n=int(input("Enter number:"))
+factorial=1
+for i in range(1,n+1):
+    factorial=factorial*i
+print("Factorial of",n,"is",factorial)
